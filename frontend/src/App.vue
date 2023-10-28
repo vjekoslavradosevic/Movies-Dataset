f6d63b035971864cffd487aa5603052b7035a91e
Vjekoslav <vr53601@fer.hr>
2023-10-28 22:15:54 +0200

    napravljen dizajn za index.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,99 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <nav>
+        <div class="nav">
+            <h1>{{ title }}</h1>
+            <div class="links">
+                <router-link :to="link.href" @click="setActive(link)" v-for="link in links"
+                    ><div class="link" :class="{ 'link-active': link.active }">
+                        {{ link.name }}
+                    </div>
+                </router-link>
+            </div>
+        </div>
+    </nav>
+    <router-view />
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "Movies Dataset",
+            links: [
+                {
+                    name: "Index",
+                    href: "/",
+                    active: true,
+                },
+                {
+                    name: "Database",
+                    href: "/database",
+                    active: false,
+                },
+            ],
+        };
+    },
+    methods: {
+        setActive(link) {
+            this.links.forEach((link) => (link.active = false));
+            link.active = true;
+        },
+    },
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+    margin: 20px;
+    font-weight: bold;
+    color: #2e2e2e;
+}
+
+div.nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #00874b;
+}
+
+div.links {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.link {
+    margin: 15px 15px 15px 0;
+    padding: 15px;
+    background-color: #00874b;
+    border-radius: 5px;
+}
+
+.link-active {
+    background-color: #10e887;
+    color: white;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #383838;
+    text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+    color: white;
 }
 </style>
--- frontend/src/views/IndexView.vue
+++ frontend/src/views/IndexView.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-        <button @click="downloadCSV">Download CSV</button>
-        <button @click="downloadJSON">Download JSON</button>
+    <div class="grid">
+        <div class="container">
+            <div class="download-link" @click="downloadCSV">Download CSV</div>
+            <p>
+                CSV, or Comma-Separated Values, is a versatile file format used for storing structured data. Each row in
+                a CSV file represents a record, with data separated by commas. While the standard delimiter is a comma,
+                other delimiters like semicolons or tabs can be used. CSV files find extensive use in data exchange and
+                storage due to their simplicity and broad support among various software applications.
+            </p>
+        </div>
+        <div class="container">
+            <div class="download-link" @click="downloadJSON">Download JSON</div>
+            <p>
+                JSON, or JavaScript Object Notation, is a widely used data interchange format. It is a lightweight,
+                human-readable format that is easy for both people and machines to understand. JSON data is structured
+                as key-value pairs, making it ideal for representing complex data structures. JSON's simplicity and
+                flexibility have led to its widespread adoption in web development, APIs, and data storage.
+            </p>
+        </div>
     </div>
 </template>
 
@@ -27,3 +43,45 @@ export default {
     },
 };
 </script>
+
+<style>
+.grid {
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: 1fr 1fr;
+    height: 40vh;
+    margin: 20px;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    border-radius: 10px;
+    border: 1px solid #a9fbd6fe;
+}
+
+.container:hover {
+    background-color: #a9fbd6fe;
+}
+
+.download-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    margin: 20px;
+    background-color: #00af60;
+    border-radius: 5px;
+}
+.download-link:hover {
+    cursor: pointer;
+    background-color: #10e887;
+}
+p {
+    margin: 20px;
+    font-size: large;
+    letter-spacing: 1px;
+}
+</style>
